d271f65632627384cbe453b4a6234815e7d90324
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-04 17:25:35 +0530

    added another spec for item - should return its correct net value when it is taxble(10% tax)

--- test/com/thoughtworks/salestax/ItemTest.java
+++ test/com/thoughtworks/salestax/ItemTest.java
@@ -5,12 +5,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ItemTest {
+
     @Test
-    public void shouldReturnTheNetAmountOfAnItemWhoseValueIs100AndIsNotTaxable(){
+    public void shouldReturnItsNetAmountWhenItsGrossValueIs100AndIsNotTaxable(){
         boolean isTaxable = false;
         boolean isImported = false;
         Item item = new Item(100, isTaxable, isImported);
 
         assertEquals(100.0, item.itemNetAmount(), 0.0);
+    }
+
+    @Test
+    public void shouldReturnItsNetAmountWhenItsGrossValueIs100AndIsTaxable() {
+        boolean isTaxable = true;
+        boolean isImported = false;
+        Item item = new Item(100, isTaxable, isImported);
+
+        assertEquals(11 0.0, item.itemNetAmount(), 0.0);
     }
 }
